c4716dcf13910ef70d2296189f211343ad7c2a53
Shreyascr5 <shreyas050801@gmail.com>
2023-03-09 13:18:01 +0530

    added date

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ var hour = document.getElementById("hrs");
 var minute = document.getElementById("min");
 var second = document.getElementById("sec");
 var ampm = document.getElementById("am");
+var date = document.getElementById("date");
+// var month = document.getElementById("month");
+// var year = document.getElementById("year");
 
 function timeStamp() {
     var getdata = new Date();
     var hr = getdata.getHours();
     var mi = getdata.getMinutes();
     var se = getdata.getSeconds();
+    var d = getdata.getDate();
+    var m = getdata.getMonth() + 1;
+    var y = getdata.getFullYear();
+
     if (hr < 10) {
         hour.innerHTML = "0" + hr;
     } else { hour.innerHTML = hr; }
@@ -26,5 +33,6 @@ function timeStamp() {
     } else {
         ampm.innerHTML = "AM";
     }
+    date.innerHTML = d + "/" + m + "/" + y;
 }
 setInterval(timeStamp, 10);
